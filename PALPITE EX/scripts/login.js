16a2457cbd3e51341924a0c9b363b7f1dba9282a
kledisom <60370756+kledisom@users.noreply.github.com>
2022-06-16 17:27:44 -0300

    inicio do projeto

---
+++ PALPITE EX/scripts/login.js
@@ -0,0 +1,17 @@
+const nome = 'teste';
+const senha = 123;
+
+
+
+
+
+function acessar() {
+	
+const user = document.getElementById('users').value;
+const pass = document.getElementById('password').value;
+	
+	if(user == nome && pass == senha) {
+		location.href="../pages/palpite.html"
+	}else{console.log('ACESSO NEGADO!')}
+	
+}
