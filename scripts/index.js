79a12ac76c7e44aef37e1a017c2ebf555e5c7b5c
kledisom <60370756+kledisom@users.noreply.github.com>
2022-06-30 21:03:41 -0300

    revisões

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ el.addEventListener("click", () => { location.href="./pages/login.html" });
 const elCont = document.getElementById('contato');
 elCont.addEventListener("click", () => { location.href="http://api.whatsapp.com/send?1=pt_BR&phone=5579988631649" });
 
-const elManut = document.getElementById('manut');
-elManut.addEventListener("click", () => { location.href="./pages/login-manut.html" });
+/*const elManut = document.getElementById('manut');
+elManut.addEventListener("click", () => { location.href="./pages/login-manut.html" });*/
 
 
--- scripts/palpite.js
+++ scripts/palpite.js
@@ -71,6 +71,8 @@ function salvar() {
         parseInt(document.querySelectorAll("input")[19].value)
       ];
 
+
+
     db.collection('john-doe').add({
         user: nome,
         time1: arr[0], time2: arr[1], time3: arr[2], time4: arr[3], time5: arr[4],
@@ -127,9 +129,38 @@ function pegarTime() {
 pegarTime()
 
 
-function refresh() {
-  
-}
+/*function verificarVazio(){
+      
+  var arr = [
+    document.querySelectorAll("input")[0].value,
+    document.querySelectorAll("input")[1].value,
+    document.querySelectorAll("input")[2].value,
+    document.querySelectorAll("input")[3].value,
+    document.querySelectorAll("input")[4].value,
+    document.querySelectorAll("input")[5].value,
+    document.querySelectorAll("input")[6].value,
+    document.querySelectorAll("input")[7].value,
+    document.querySelectorAll("input")[8].value,
+    document.querySelectorAll("input")[9].value,
+    document.querySelectorAll("input")[10].value,
+    document.querySelectorAll("input")[11].value,
+    document.querySelectorAll("input")[12].value,
+    document.querySelectorAll("input")[13].value,
+    document.querySelectorAll("input")[14].value,
+    document.querySelectorAll("input")[15].value,
+    document.querySelectorAll("input")[16].value,
+    document.querySelectorAll("input")[17].value,
+    document.querySelectorAll("input")[18].value,
+    document.querySelectorAll("input")[19].value
+  ];
+       if(arr[0] || arr[1] || arr[1] || arr[1] || arr[1] || arr[1] || 
+        arr[1] || arr[1] || arr[1] || arr[1] || arr[1] || arr[1] || arr[1]
+         || arr[1] || arr[1] || arr[1] || arr[1] || arr[1] || arr[1] == '') {
+        
+          swal("Ops. Ainda tem campo fazio,  Por favor preencha todos os campos!");
+       }else{};
+      
+}*/
 
 
 
@@ -157,60 +188,3 @@ function refresh() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-/* function save() {
-let time1 = document.querySelectorAll("input")[0].value;
-let time2 = document.querySelectorAll("input")[1].value;
-let time3 = document.querySelectorAll("input")[2].value;
-let time4 = document.querySelectorAll("input")[3].value;
-let time5 = document.querySelectorAll("input")[4].value;
-let time6 = document.querySelectorAll("input")[5].value;
-let time7 = document.querySelectorAll("input")[6].value;
-let time8 = document.querySelectorAll("input")[7].value;
-let time9 = document.querySelectorAll("input")[8].value;
-let time10 = document.querySelectorAll("input")[9].value;
-let time11 = document.querySelectorAll("input")[10].value;
-let time12 = document.querySelectorAll("input")[11].value;
-let time13 = document.querySelectorAll("input")[12].value;
-let time14 = document.querySelectorAll("input")[13].value;
-let time15 = document.querySelectorAll("input")[14].value;
-let time16 = document.querySelectorAll("input")[15].value;
-let time17 = document.querySelectorAll("input")[16].value;
-let time18 = document.querySelectorAll("input")[17].value;
-let time19 = document.querySelectorAll("input")[18].value;
-
-console.log(time1,time2);
-
-localStorage.setItem('timeA1', time1);
-localStorage.setItem('timeA2', time2);
-localStorage.setItem('timeA3', time3);
-localStorage.setItem('timeA4', time4);
-localStorage.setItem('timeA5', time5);
-localStorage.setItem('timeA6', time6);
-localStorage.setItem('timeA7', time7);
-localStorage.setItem('timeA8', time8);
-localStorage.setItem('timeA9', time9);
-localStorage.setItem('timeA10', time10);
-localStorage.setItem('timeA11', time11);
-localStorage.setItem('timeA12', time12);
-localStorage.setItem('timeA13', time13);
-localStorage.setItem('timeA14', time14);
-localStorage.setItem('timeA15', time15);
-localStorage.setItem('timeA16', time16);
-localStorage.setItem('timeA17', time17);
-localStorage.setItem('timeA18', time18);
-
-};
-
- */
